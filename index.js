1fe7e7861c95e1a89f12b46fdcab7d5fc54ff9f7
tgunnr <finalresidentevilfantasy@gmail.com>
2024-08-21 04:52:59 -0700

    exercise 10-2

--- index.js
+++ index.js
@@ -150,3 +150,17 @@ var LANG = localLangConfig || 'en'
 
 // Log the result
 console.log('Language setting:', LANG)
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+
+var USER_THEME = userSavedTheme || 'light'
+
+// Log the result
+console.log('User theme setting:', USER_THEME);
