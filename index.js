ec076d46028194152affc519733878c1a5f1281c
tgunnr <finalresidentevilfantasy@gmail.com>
2024-08-21 03:54:05 -0700

    exercise 4

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ console.log(nums2)
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
 
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
 
-const [toppingOne, toppingTwo] = pizzaToppings
+// const [toppingOne, toppingTwo] = pizzaToppings
 
-console.log(toppingOne)
-console.log(toppingTwo)
+// console.log(toppingOne)
+// console.log(toppingTwo)
 
 // Exercise 3
 
@@ -42,3 +42,15 @@ const { make, model } = car
 
 console.log(make)
 console.log(model)
+
+// Exercise 4
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const controversialPizzaToppings = [...pizzaToppings]
+
+console.log(controversialPizzaToppings)
